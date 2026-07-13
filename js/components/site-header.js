dcbdf829ba5b725696b0f07f5b40a64dac709fc1
rafaeuclydes <rafael.euclydes@gmail.com>
2026-07-13 17:37:08 -0300

    fix(paths): adjust header image and navigation links for root index

--- js/components/site-header.js
+++ js/components/site-header.js
@@ -1,13 +1,13 @@
 class SiteHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `<header class="cabecalho">
-        <a href="/index.html">
-        <img src="/assets/image/logo_miaudex.png" alt="Loco da Miaudex" />
+        <a href="./index.html">
+        <img src="./assets/image/logo_miaudex.png" alt="Logo da Miaudex" />
         </a>
         <nav class="menu">
-        <a href="/index.html">MiauDex</a>
-        <a href="/pages/ong.html">Ajude uma ONG</a>
-        <a href="/pages/contact.html">Contato</a>
+        <a href="./index.html">MiauDex</a>
+        <a href="./pages/ong.html">Ajude uma ONG</a>
+        <a href="./pages/contact.html">Contato</a>
         </nav>
         </header>`;
   }
